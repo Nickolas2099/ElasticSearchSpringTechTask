3728241b721db1131595f7c58a482c2d247ca5e4
Nickolas2099 <iantoshka2003@mail.ru>
2024-10-17 16:39:51 +0300

    add docker compose with docker & elastic images

---
+++ src/main/java/com/example/searchTechTask/SearchTechTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.searchTechTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchTechTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchTechTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/searchTechTask/SearchTechTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.searchTechTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchTechTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
